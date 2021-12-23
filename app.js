72984d1fd53ce7e83df746f559e3a8ecf29f36fa
Canzalo <44494765+canzalo@users.noreply.github.com>
2021-12-23 13:18:20 +0300

    app.js

--- app.js
+++ app.js
@@ -1,23 +1,4 @@
-/*
-var btc = document.getElementById('bitcoin');
-var eth = document.getElementById('ethereum');
-var doge = document.getElementById('dogecoin');
-let settings = {
-    "async": true,
-    "scrossDomain": true,
-    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
-    "method": "GET",
-    "headers": {}
-}
 
-
-$.ajax(settings).done(function(response) {
-    btc.innerHTML = response.bitcoin.usd;
-    eth.innerHTML = response.ethereum.usd;
-    doge.innerHTML = response.dogecoin.usd;
-
-});
-*/
 
 
 fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd').then((data) => {
